9660e3943a37279ede8e79fd299ccfa01fa685e8
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2019-10-22 01:26:00 +0700

    Add Model Files

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'hola';
+});
+
+Route::get('/test', function() {
+	return 'coba';
 });
